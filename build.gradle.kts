8c5fccef36cd06437a3ee5b12cb6fdf235ffe4ff
Brian Estrada <lovesguitar@gmail.com>
2025-03-20 14:49:36 -0400

    fix: downgraded kotlin dep version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "2.1.20" apply false
+    kotlin("multiplatform") version "2.1.10" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0" apply false
     id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
 }
